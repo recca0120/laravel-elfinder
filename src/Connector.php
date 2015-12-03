25f09e948b87eaa7fbf03e72e79d60b1532f037a
unknown <recca0120@gmail.com>
2015-12-04 05:30:40 +0800

    refactor

--- src/Connector.php
+++ src/Connector.php
@@ -18,36 +18,34 @@ class Connector extends elFinderConnector
             @ob_end_clean();
         }
 
-        $header = isset($data['header']) ? $data['header'] : $this->header;
+        $temp = isset($data['header']) ? (array) $data['header'] : (array) $this->header;
         unset($data['header']);
 
         $headers = [];
-        if ($header) {
-            foreach ((array) $header as $headerString) {
-                if (strpos($headerString, ':') !== false) {
-                    list($key, $value) = explode(':', $headerString, 2);
-                    $headers[$key] = $value;
-                }
+        foreach ($temp as $header) {
+            if (strpos($header, ':') !== false) {
+                list($key, $value) = explode(':', $header, 2);
+                $headers[$key] = $value;
             }
         }
 
         $headers['Access-Control-Allow-Origin'] = '*';
 
         if (isset($data['pointer']) === true) {
-            $this->response = new StreamedResponse(function () use ($data) {
+            return $this->response = new StreamedResponse(function () use ($data) {
                 rewind($data['pointer']);
                 fpassthru($data['pointer']);
                 if (empty($data['volume']) === false) {
                     $data['volume']->close($data['pointer'], $data['info']['hash']);
                 }
             }, 200, $headers);
-        } else {
-            if (empty($data['raw']) === false && empty($data['error']) === false) {
-                $this->response = new JsonResponse($data['error'], 500);
-            } else {
-                $this->response = new JsonResponse($data, 200, $headers);
-            }
         }
+
+        if (empty($data['raw']) === false && empty($data['error']) === false) {
+            return $this->response = new JsonResponse($data['error'], 500);
+        }
+
+        return $this->response = new JsonResponse($data, 200, $headers);
     }
 
     public function run()
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,7 +9,18 @@ class ServiceProvider extends BaseServiceProvider
 {
     protected $namespace = 'Recca0120\Elfinder\Http\Controllers';
 
+    protected $prefix = 'elfinder';
+
+    protected $router;
+
     public function boot(Router $router)
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'elfinder');
+        $this->publishAssets();
+        $this->bootRoutes($router);
+    }
+
+    public function register()
     {
         // require __DIR__.'/../php/elFinderConnector.class.php';
         // require __DIR__.'/../php/elFinder.class.php';
@@ -22,27 +33,22 @@ class ServiceProvider extends BaseServiceProvider
         if (! defined('ELFINDER_IMG_PARENT_URL')) {
             define('ELFINDER_IMG_PARENT_URL', asset('vendor/elfinder'));
         }
-
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'elfinder');
-        $this->map($router);
-        $this->publish();
     }
 
-    protected function map($router)
+    protected function bootRoutes($router)
     {
         if ($this->app->routesAreCached() === false) {
-            $prefix = 'elfinder';
             $group = $router->group([
                 'namespace' => $this->namespace,
                 'as' => 'elfinder::',
-                'prefix' => $prefix,
+                'prefix' => $this->prefix,
             ], function () {
                 require __DIR__.'/Http/routes.php';
             });
         }
     }
 
-    protected function publish()
+    protected function publishAssets()
     {
         $this->publishes([
             __DIR__.'/../resources/views' => base_path('resources/views/vendor/elfinder'),
@@ -51,9 +57,5 @@ class ServiceProvider extends BaseServiceProvider
         $this->publishes([
             __DIR__.'/../resources/assets' => public_path('vendor/elfinder'),
         ], 'public');
-    }
-
-    public function register()
-    {
     }
 }
