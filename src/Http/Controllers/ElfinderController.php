b1621ef4fd63a0353ee5412b60ce37ec387c2cda
unknown <recca0120@gmail.com>
2015-12-11 04:36:20 +0800

    # This is a combination of 2 commits. # The first commit's message is:
    support laravel 5.2
    
    # The 2nd commit message will be skipped:
    
    #	custom root path

--- src/Http/Controllers/ElfinderController.php
+++ src/Http/Controllers/ElfinderController.php
@@ -2,10 +2,11 @@
 
 namespace Recca0120\Elfinder\Http\Controllers;
 
+use Closure;
 use elFinder;
 use File;
-use Recca0120\Elfinder\Connector;
 use Illuminate\Routing\Controller;
+use Recca0120\Elfinder\Connector;
 
 class ElfinderController extends Controller
 {
@@ -16,64 +17,118 @@ class ElfinderController extends Controller
 
     public function connector()
     {
-        $dirs = collect();
+        $config = config('elfinder');
 
-        if (auth()->check() === true) {
-            $dirs->push([
-                'alias' => 'Home',
-                'icon' => null,
-                'path' => 'upload/filemanager/user/'.auth()->id(),
-            ]);
+        $roots = array_get($config, 'roots', []);
 
-            $dirs->push([
-                'alias' => 'Shared',
-                'path' => 'upload/filemanager/shared',
-            ]);
-        }
+        foreach ($roots as $key => $root) {
+            $root['driver'] = (empty($root['driver']) === true) ? 'LocalFileSystem' : $root['driver'];
 
-        $dirs = $dirs->map(function($item) {
-            $driver = array_get($item, 'driver', 'LocalFileSystem');
-            $path = trim(array_get($item, 'path', 'upload/filemanager/shared'), '/');
-            $options = array_merge($item, [
-                'driver' => $driver,
-                'accessControl' => function ($attr, $path, $data, $volume, $isDir) {
-                    return strpos(basename($path), '.') === 0
-                        ? ! ($attr == 'read' || $attr == 'write')
-                        :  null;
-                },
-            ]);
+            if (empty($root['path']) === false && ($root['path'] instanceof Closure) === true) {
+                $root['path'] = call_user_func($root['path']);
+            }
 
-            switch ($driver) {
+            switch ($root['driver']) {
                 case 'LocalFileSystem':
-                    if (File::exists(public_path($path)) === false) {
-                        File::makeDirectory(public_path($path), 0755, true);
+                    if (empty($root['URL']) === true) {
+                        $root['URL'] = url(substr($root['path'], strlen(public_path()) + 1));
+                    } elseif (($root['URL'] instanceof Closure) === true) {
+                        $root['URL'] = call_user_func($root['URL']);
                     }
-
-                    if (empty($options['URL'])) {
-                        $options['URL'] = url($path);
-                    }
-
-                    if (empty($options['tmpPath']) === true) {
-                        $options['tmpPath'] = $path.'/.tmp';
-                    }
-
+                    $root = array_merge([
+                        'mimeDetect' => 'internal',
+                        'tmpPath' => '.tmb',
+                        'utf8fix' => true,
+                        'tmbCrop' => false,
+                        'tmbBgColor' => 'transparent',
+                        'accessControl' => function ($attr, $path, $data, $volume, $isDir) {
+                            return strpos(basename($path), '.') === 0       // if file/folder begins with '.' (dot)
+                                ? ! ($attr == 'read' || $attr == 'write')    // set read+write to false, other (locked+hidden) set to true
+                                :  null;                                    // else elFinder decide it itself
+                        },
+                    ], $root);
                     break;
             }
 
-            return $options;
-        });
-
-        $opts = [
-            // 'debug' => true,
-            'roots' => $dirs->toArray(),
-        ];
-
-        $connector = new Connector(new elFinder($opts));
+            class_exists('elFinderVolume'.$root['driver']);
+            $roots[$key] = $root;
+        }
+        $config['roots'] = $roots;
+        $connector = new Connector(new elFinder($config));
 
         return $connector->run();
+
+        // 'driver' => 'LocalFileSystem',
+        // 'path' => public_path('media/elfinder/shared'),
+        // 'URL' => url('media/elfinder/shared'),
+        // // 'treeDeep' => 3,
+        // // 'alias' => 'File system',
+        // 'mimeDetect' => 'internal',
+        // 'tmpPath' => '.tmb',
+        // 'utf8fix' => true,
+        // 'tmbCrop' => false,
+        // 'tmbBgColor' => 'transparent',
+        // 'accessControl' => 'access',
+        // 'acceptedName' => '/^[^\.].*$/',
+        // // 'disabled' => array('extract', 'archive'),
+        // // 'tmbSize' => 128,
+        // 'attributes' => [[
+        //     'pattern' => '/\.js$/',
+        //     'read' => true,
+        //     'write' => false,
+        // ],[
+        //     'pattern' => '/^\/icons$/',
+        //     'read' => true,
+        //     'write' => false,
+        // ]],
+        // // All Mimetypes not allowed to upload
+        // 'uploadDeny' => ['all'],
+        // // Mimetype `image` and `text/plain` allowed to upload
+        // 'uploadAllow' => ['image', 'text/plain'],
+        // // allowed Mimetype `image` and `text/plain` only
+        // 'uploadOrder' => ['deny', 'allow'],
+
+        // $dirs = $dirs->map(function ($item) use ($root, $path) {
+        //     $driver = array_get($item, 'driver', 'LocalFileSystem');
+        //     $path = array_get($item, 'path', $path.'/shared', '/');
+
+        //     $options = array_merge($item, [
+        //         'driver' => $driver,
+        //         'accessControl' => function ($attr, $path, $data, $volume, $isDir) {
+        //             return strpos(basename($path), '.') === 0
+        //                 ? ! ($attr == 'read' || $attr == 'write')
+        //                 :  null;
+        //         },
+        //     ]);
+
+        //     switch ($driver) {
+        //         case 'LocalFileSystem':
+        //             if (File::exists(public_path($path)) === false) {
+        //                 File::makeDirectory(public_path($path), 0755, true);
+        //             }
+
+        //             if (empty($options['URL'])) {
+        //                 $options['URL'] = url($path);
+        //             }
+
+        //             if (empty($options['tmpPath']) === true) {
+        //                 $options['tmpPath'] = $path.'/.tmp';
+        //             }
+
+        //             break;
+        //     }
+
+        //     return $options;
+        // });
     }
 
-    public function sound($file) {
-        return response()->download(__DIR__.'/../../../resources/assets/sounds/rm.wav');
+    public function sound($file)
+    {
+        $file = __DIR__.'/../../../resources/assets/sounds/'.$file;
+        $mimeType = File::mimeType($file);
+
+        return response(file_get_contents($file), 200, [
+            'content-type' => $mimeType,
+        ]);
     }
 }
