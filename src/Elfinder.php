ab44cea986f621e2c41f9441512e09af339b4a87
Recca Tsai <recca0120.gmail.com>
2016-11-25 11:36:04 +0800

    refactor

--- src/Elfinder.php
+++ src/Elfinder.php
@@ -2,8 +2,87 @@
 
 namespace Recca0120\Elfinder;
 
+use Closure;
+use elFinder as BaseElfinder;
+use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
 class Elfinder
 {
+    /**
+     * connector.
+     *
+     * @param \Recca0120\Elfinder\Session                  $session
+     * @param \Illuminate\Http\Request                     $request
+     * @param \Illuminate\Filesystem\Filesystem            $filesystem
+     * @param \Illuminate\Contracts\Routing\UrlGenerator   $urlGenerator
+     * @param array                                        $config
+     *
+     * @return mixed
+     */
+    public function __construct(
+        Session $session,
+        Request $request,
+        Filesystem $filesystem,
+        UrlGenerator $urlGenerator,
+        $config = []
+    ) {
+        $this->filesystem = $filesystem;
+        $this->urlGenerator = $urlGenerator;
+        $this->session = $session;
+        $this->request = $request;
+        $this->config = $config;
+    }
+
+    public function getConnector()
+    {
+        $options = Arr::get($this->config, 'options', []);
+        $user = $this->request->user();
+        $accessControl = Arr::get($this->config, 'accessControl');
+
+        $roots = Arr::get($options, 'roots', []);
+        foreach ($roots as $key => $disk) {
+            $disk['driver'] = (empty($disk['driver']) === true) ? 'LocalFileSystem' : $disk['driver'];
+            $disk['autoload'] = true;
+
+            if (empty($disk['path']) === false && ($disk['path'] instanceof Closure) === true) {
+                $disk['path'] = call_user_func($disk['path']);
+            }
+
+            switch ($disk['driver']) {
+                case 'LocalFileSystem':
+                    if (strpos($disk['path'], '{user_id}') !== -1 && is_null($user) === true) {
+                        continue;
+                    }
+                    $userId = $user->id;
+                    $disk['path'] = str_replace('{user_id}', $userId, $disk['path']);
+                    $disk['URL'] = $this->urlGenerator->to(str_replace('{user_id}', $userId, $disk['URL']));
+
+                    if ($this->filesystem->exists($disk['path']) === false) {
+                        $this->filesystem->makeDirectory($disk['path'], 0755, true);
+                    }
+
+                    $disk = array_merge([
+                        'mimeDetect' => 'internal',
+                        'utf8fix' => true,
+                        'tmbCrop' => false,
+                        'tmbBgColor' => 'transparent',
+                        'accessControl' => $accessControl,
+                    ], $disk);
+                    break;
+            }
+            $roots[$key] = $disk;
+        }
+        $options = array_merge($options, [
+            'roots' => $roots,
+            'session' => $this->session,
+        ]);
+
+        return new Connector(new BaseElfinder($options));
+    }
+
     public static function access($attr, $path, $data, $volume, $isDir)
     {
         return strpos(basename($path), '.') === 0       // if file/folder begins with '.' (dot)
---
+++ src/ElfinderServiceProvider.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Recca0120\Elfinder;
+
+use Illuminate\Routing\Router;
+use Illuminate\Support\Arr;
+use Illuminate\Support\ServiceProvider;
+
+class ElfinderServiceProvider extends ServiceProvider
+{
+    /**
+     * namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'Recca0120\Elfinder\Http\Controllers';
+
+    /**
+     * handle routes.
+     *
+     * @param \Illuminate\Routing\Router $router
+     *
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        if ($this->app->runningInConsole() === true) {
+            $this->handlePublishes();
+
+            return;
+        }
+
+        $config = $this->app['config']->get('elfinder', []);
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'elfinder');
+        $this->handleRoutes($router, $config);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/elfinder.php', 'elfinder');
+
+        $this->app->singleton(Elfinder::class, function ($app) {
+            $session = new Session($app['session']);
+            $config = $app['config']['elfinder'];
+
+            return new Elfinder($session, $app['request'], $app['files'], $app['url'], $config);
+        });
+    }
+
+    /**
+     * register routes.
+     *
+     * @param \Illuminate\Routing\Router $router
+     * @oaram array                      $config
+     *
+     * @return void
+     */
+    protected function handleRoutes(Router $router, $config)
+    {
+        if ($this->app->routesAreCached() === false) {
+            $router->group(array_merge([
+                'middleware' => ['web', 'auth'],
+                'prefix' => 'elfinder',
+                'as' => 'elfinder.',
+                'namespace' => $this->namespace,
+            ], Arr::get($config, 'elfinder.route', [])), function () {
+                require __DIR__.'/Http/routes.php';
+            });
+        }
+    }
+
+    /**
+     * handle publishes.
+     *
+     * @return void
+     */
+    protected function handlePublishes()
+    {
+        $this->publishes([
+            __DIR__.'/../resources/views' => $this->app->basePath().'/resources/views/vendor/elfinder',
+        ], 'views');
+
+        $this->publishes([
+            __DIR__.'/../resources/elfinder' => $this->app->publicPath().'/vendor/elfinder',
+        ], 'public');
+
+        $this->publishes([
+            __DIR__.'/../config/elfinder.php' => $this->app->configPath().'/elfinder.php',
+        ], 'config');
+    }
+}
